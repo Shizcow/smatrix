895a9df7a4f6986be6741d65c5f3dd9bb0d7fd24
Shizcow <pohl.devin@gmail.com>
2020-03-18 10:43:36 -0600

    Proof of concept done for getting stock prices

--- src/requests.rs
+++ src/requests.rs
@@ -4,7 +4,33 @@
  * Handles requests to get stock prices 
  * 
  */
+use curl::easy::Easy;
+use std::str;
+
 
 fn main(){
-    println!("request");
+
+    let mut dst = Vec::new();
+    {
+	let mut easy = Easy::new();
+	easy.url("https://query1.finance.yahoo.com/v7/finance/quote?symbols=TSLA,AMD").unwrap();
+	
+	let mut transfer = easy.transfer();
+	transfer.write_function(|data| {
+            dst.extend_from_slice(data);
+            Ok(data.len())
+	}).unwrap();
+	transfer.perform().unwrap();
+    }
+
+    let pre_parsed = str::from_utf8(&dst).unwrap();
+    let parsed = json::parse(pre_parsed).unwrap();
+
+    let results = &parsed["quoteResponse"]["result"];
+    for i in 0..results.len() {
+	println!("Ticker: {:}", results[i]["symbol"]);
+	println!("Price: {:}", results[i]["regularMarketPrice"]);
+	println!("Change: {:}", results[i]["regularMarketChange"]);
+	println!("Change Percet: {:}%", results[i]["regularMarketChangePercent"]);
+    }
 }
